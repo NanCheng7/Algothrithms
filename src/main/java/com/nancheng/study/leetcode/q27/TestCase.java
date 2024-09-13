2d63760f502653c02e771beb0390a2eec46977a3
NanCheng7 <3150744212@qq.com>
2024-09-13 17:10:18 +0800

    Q27 Complete!

---
+++ src/main/java/com/nancheng/study/leetcode/q27/TestCase.java
@@ -0,0 +1,12 @@
+package com.nancheng.study.leetcode.q27;
+
+import org.junit.Test;
+
+public class TestCase {
+    @Test
+    public void testCase1(){
+        int[] nums1 = {3,2,2,3};
+        int val = 3;
+        System.out.println(new Solution().removeElement(nums1,val));
+    }
+}
