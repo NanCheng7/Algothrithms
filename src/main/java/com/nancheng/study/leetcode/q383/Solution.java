d4f1cd7833448a8913d505d222d6eda3edbf4c22
NanCheng <nanchengqj@gmail.com>
2024-10-12 19:36:26 +0800

    Q383 Complete!

--- src/main/java/com/nancheng/study/leetcode/q383/Solution.java
+++ src/main/java/com/nancheng/study/leetcode/q383/Solution.java
@@ -2,6 +2,8 @@ package com.nancheng.study.leetcode.q383;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 /**
  * @author: NanCheng
  * @email: nanchengqj@gmail.com
@@ -13,17 +15,31 @@ public class Solution {
     @Test
     public void test() {
         char a = 'a';
+        int[]map = new int[26];
         System.out.println((int) a);
     }
 
     public boolean canConstruct(String ransomNote, String magazine) {
         final int prefix = 97;
-        int[][] map = new int[2][26];
+        int[]map = new int[26];
         if (ransomNote.length() > magazine.length()) {
             return false;
         }
-        for (int i = 0; i < magazine.length(); i++) {
-
+        int i = 0;
+        while( i < ransomNote.length()) {
+            // 遍历字符
+            map[ransomNote.charAt(i) - prefix]--;
+            map[magazine.charAt(i) - prefix]++;
+            i++;
+        }
+        while (i < magazine.length()) {
+            map[ransomNote.charAt(i) - prefix]++;
+            i++;
+        }
+        for (int j : map) {
+            if (j < 0) {
+                return false;
+            }
         }
         return true;
     }
